b8b163e7113afb86bbf7edbb761710ebc24350e7
victorchu8 <victorchu1996@gmail.com>
2018-06-04 17:24:54 -0600

    Clean up some functions

--- flux/Controller/HomeVC.swift
+++ flux/Controller/HomeVC.swift
@@ -17,26 +17,18 @@ class HomeVC: UIViewController, ChartViewDelegate {
     
     var ref:DatabaseReference?
     
-    var mondayDictionary = [String: Double]()
-    var tuesdayDictionary = [String: Double]()
-    var wednesdayDictionary = [String: Double]()
-    var thursdayDictionary = [String: Double]()
-    var fridayDictionary = [String: Double]()
-    var saturdayDictionary = [String: Double]()
-    var sundayDictionary = [String: Double]()
-    
     var hourOfDay = [String]()
     var numberOfEntriesArray = [Double]()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        retrieveData(Day: determineDay())
+        retrieveDataForDay(Day: determineDay())
         
     }
     @IBAction func reloadButtonTapped(_ sender: Any) {
         numberOfEntriesArray.removeAll()
-        retrieveData(Day: determineDay())
+        retrieveDataForDay(Day: determineDay())
      }
     
     func lineChartProperties() {
@@ -51,307 +43,90 @@ class HomeVC: UIViewController, ChartViewDelegate {
         lineChartView.rightAxis.drawAxisLineEnabled = false
         lineChartView.legend.enabled = false
         lineChartView.backgroundColor = #colorLiteral(red: 0.9764705882, green: 0.9764705882, blue: 0.9764705882, alpha: 1)
+        lineChartView.xAxis.labelTextColor = #colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)
         lineChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
     }
-
     
-    func retrieveData(Day: String) {
+    func retrieveDataForDay(Day: String) {
         
         // Set the firebase reference
         ref = Database.database().reference().child("charts").child(Day)
         
-        if Day == "monday" {
-            ref?.observeSingleEvent(of: .value, with: { (snapshot) in
-                
-                let groupKeys = snapshot.children.compactMap { $0 as? DataSnapshot }.map { $0.key }
-                
-                // This group will keep track of the number of blocks still pending
-                let group = DispatchGroup()
-                
-                for child in snapshot.children {
-                    let snap = child as! DataSnapshot
-                    let key = snap.key
-                    let value = snap.value as! Double
-                    
-                    self.mondayDictionary[key] = value
-                    // self.hourOfDay.append(key)
-                    self.numberOfEntriesArray.append(value)
-                    
-                    
-                    print(self.mondayDictionary[key]!)
-                    
-                    print("key = \(key) value = \(value)")
-                }
-                
-                for groupKey in groupKeys {
-                    group.enter()
-                    self.ref?.child("groups").child(groupKey).child("name").observeSingleEvent(of: .value, with: { snapshot in
-                        group.leave()
-                    })
-                }
-                
-                // We ask to be notified when every block left the group
-                group.notify(queue: .main) {
-                    print("All callbacks are completed")
-
-                    self.lineChartProperties()
-                    self.lineChartView.setBarChartData(xValues: self.hourOfDay, yValues: self.numberOfEntriesArray, label: "Number of Entries")
-                }
-            })
-        
-        }
-        
-        else if Day == "tuesday" {
-            ref?.observeSingleEvent(of: .value, with: { (snapshot) in
-                
-                let groupKeys = snapshot.children.compactMap { $0 as? DataSnapshot }.map { $0.key }
-                
-                // This group will keep track of the number of blocks still pending
-                let group = DispatchGroup()
-                
-                for child in snapshot.children {
-                    let snap = child as! DataSnapshot
-                    let key = snap.key
-                    let value = snap.value as! Double
-                    
-                    self.tuesdayDictionary[key] = value
-                    // self.hourOfDay.append(key)
-                    self.numberOfEntriesArray.append(value)
-                    
-                    
-                    print(self.tuesdayDictionary[key]!)
-                    
-                    print("key = \(key) value = \(value)")
-                }
-                
-                for groupKey in groupKeys {
-                    group.enter()
-                    self.ref?.child("groups").child(groupKey).child("name").observeSingleEvent(of: .value, with: { snapshot in
-                        group.leave()
-                    })
-                }
-                
-                // We ask to be notified when every block left the group
-                group.notify(queue: .main) {
-                    print("All callbacks are completed")
-                    
-                    self.lineChartProperties()
-                    self.lineChartView.setBarChartData(xValues: self.hourOfDay, yValues: self.numberOfEntriesArray, label: "Number of Entries")
-                    
-                }
-            })
+        ref?.observeSingleEvent(of: .value, with: { (snapshot) in
             
-        }
-        
-        else if Day == "wednesday" {
-            ref?.observeSingleEvent(of: .value, with: { (snapshot) in
+            let groupKeys = snapshot.children.compactMap { $0 as? DataSnapshot }.map { $0.key }
+            
+            // This group will keep track of the number of blocks still pending
+            let group = DispatchGroup()
+            
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let key = snap.key
+                let value = snap.value as! Double
                 
-                let groupKeys = snapshot.children.compactMap { $0 as? DataSnapshot }.map { $0.key }
+                self.numberOfEntriesArray.append(value)
                 
-                // This group will keep track of the number of blocks still pending
-                let group = DispatchGroup()
+                //print(self.mondayDictionary[key]!)
                 
-                for child in snapshot.children {
-                    let snap = child as! DataSnapshot
-                    let key = snap.key
-                    let value = snap.value as! Double
-                    
-                    self.wednesdayDictionary[key] = value
-                    // self.hourOfDay.append(key)
-                    self.numberOfEntriesArray.append(value)
-                    
-                    
-                    print(self.wednesdayDictionary[key]!)
-                    
-                    print("key = \(key) value = \(value)")
-                }
+                print("key = \(key) value = \(value)")
+            }
+            
+            for groupKey in groupKeys {
+                group.enter()
+                self.ref?.child("groups").child(groupKey).child("name").observeSingleEvent(of: .value, with: { snapshot in
+                    group.leave()
+                })
+            }
+            
+            // We ask to be notified when every block left the group
+            group.notify(queue: .main) {
+                print("All callbacks are completed")
                 
-                for groupKey in groupKeys {
-                    group.enter()
-                    self.ref?.child("groups").child(groupKey).child("name").observeSingleEvent(of: .value, with: { snapshot in
-                        group.leave()
-                    })
-                }
-                
-                // We ask to be notified when every block left the group
-                group.notify(queue: .main) {
-                    
-                    print("All callbacks are completed")
-                    
-                    self.lineChartProperties()
-                    self.lineChartView.setBarChartData(xValues: self.hourOfDay, yValues: self.numberOfEntriesArray, label: "Number of Entries")
-                    
-                }
-            })
-        }
-        
-        else if Day == "thursday" {
-            ref?.observeSingleEvent(of: .value, with: { (snapshot) in
-                
-                let groupKeys = snapshot.children.compactMap { $0 as? DataSnapshot }.map { $0.key }
-                
-                // This group will keep track of the number of blocks still pending
-                let group = DispatchGroup()
-                
-                for child in snapshot.children {
-                    let snap = child as! DataSnapshot
-                    let key = snap.key
-                    let value = snap.value as! Double
-                    
-                    self.thursdayDictionary[key] = value
-                    // self.hourOfDay.append(key)
-                    self.numberOfEntriesArray.append(value)
-                    
-                    
-                    print(self.thursdayDictionary[key]!)
-                    
-                    print("key = \(key) value = \(value)")
-                }
-                
-                for groupKey in groupKeys {
-                    group.enter()
-                    self.ref?.child("groups").child(groupKey).child("name").observeSingleEvent(of: .value, with: { snapshot in
-                        group.leave()
-                    })
-                }
-                
-                // We ask to be notified when every block left the group
-                group.notify(queue: .main) {
-                    print("All callbacks are completed")
-                    self.lineChartProperties()
-                    self.lineChartView.setBarChartData(xValues: self.hourOfDay, yValues: self.numberOfEntriesArray, label: "Number of Entries")
-                    
-                }
-            })
-        }
-        
-        else if Day == "friday" {
-            ref?.observeSingleEvent(of: .value, with: { (snapshot) in
-                
-                let groupKeys = snapshot.children.compactMap { $0 as? DataSnapshot }.map { $0.key }
-                
-                // This group will keep track of the number of blocks still pending
-                let group = DispatchGroup()
-                
-                for child in snapshot.children {
-                    let snap = child as! DataSnapshot
-                    let key = snap.key
-                    let value = snap.value as! Double
-                    
-                    self.fridayDictionary[key] = value
-                    // self.hourOfDay.append(key)
-                    self.numberOfEntriesArray.append(value)
-                    
-                    
-                    print(self.fridayDictionary[key]!)
-                    
-                    print("key = \(key) value = \(value)")
-                }
-                
-                for groupKey in groupKeys {
-                    group.enter()
-                    self.ref?.child("groups").child(groupKey).child("name").observeSingleEvent(of: .value, with: { snapshot in
-                        group.leave()
-                    })
-                }
-                
-                // We ask to be notified when every block left the group
-                group.notify(queue: .main) {
-                    print("All callbacks are completed")
-                    self.lineChartProperties()
-                    self.lineChartView.setBarChartData(xValues: self.hourOfDay, yValues: self.numberOfEntriesArray, label: "Number of Entries")
-                    
-                }
-            })
-        }
-        
-        else if Day == "saturday" {
-            ref?.observeSingleEvent(of: .value, with: { (snapshot) in
-                
-                let groupKeys = snapshot.children.compactMap { $0 as? DataSnapshot }.map { $0.key }
-                
-                // This group will keep track of the number of blocks still pending
-                let group = DispatchGroup()
-                
-                for child in snapshot.children {
-                    let snap = child as! DataSnapshot
-                    let key = snap.key
-                    let value = snap.value as! Double
-                    
-                    self.sundayDictionary[key] = value
-                    // self.hourOfDay.append(key)
-                    self.numberOfEntriesArray.append(value)
-                    
-                    
-                    print(self.saturdayDictionary[key]!)
-                    
-                    print("key = \(key) value = \(value)")
-                }
-                
-                for groupKey in groupKeys {
-                    group.enter()
-                    self.ref?.child("groups").child(groupKey).child("name").observeSingleEvent(of: .value, with: { snapshot in
-                        group.leave()
-                    })
-                }
-                
-                // We ask to be notified when every block left the group
-                group.notify(queue: .main) {
-                    print("All callbacks are completed")
-                    self.lineChartProperties()
-                    self.lineChartView.setBarChartData(xValues: self.hourOfDay, yValues: self.numberOfEntriesArray, label: "Number of Entries")
-                    
-                }
-            })
-        }
-        
-        else {
-            ref?.observeSingleEvent(of: .value, with: { (snapshot) in
-                
-                let groupKeys = snapshot.children.compactMap { $0 as? DataSnapshot }.map { $0.key }
-                
-                // This group will keep track of the number of blocks still pending
-                let group = DispatchGroup()
-                
-                for child in snapshot.children {
-                    let snap = child as! DataSnapshot
-                    let key = snap.key
-                    let value = snap.value as! Double
-                    
-                    self.sundayDictionary[key] = value
-                   // self.hourOfDay.append(key)
-                    self.numberOfEntriesArray.append(value)
-                    
-                    
-                    print(self.sundayDictionary[key]!)
-                    
-                    print("key = \(key) value = \(value)")
-                }
-                
-                for groupKey in groupKeys {
-                    group.enter()
-                    self.ref?.child("groups").child(groupKey).child("name").observeSingleEvent(of: .value, with: { snapshot in
-                        group.leave()
-                    })
-                }
-                
-                // We ask to be notified when every block left the group
-                group.notify(queue: .main) {
-                    print("All callbacks are completed")
-                    self.lineChartProperties()
-                    self.lineChartView.setBarChartData(xValues: self.hourOfDay, yValues: self.numberOfEntriesArray, label: "Number of Entries")
+                self.retrieveHoursOfDay(Day: Day)
+            }
+        })
 
-                }
-            })
-        }
-        
         
     }
     
-
-    
-    
- 
+    func retrieveHoursOfDay(Day: String) {
+        
+        // Set the firebase reference
+        ref = Database.database().reference().child("charts").child("hoursForDay").child(Day)
+        
+        ref?.observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            let groupKeys = snapshot.children.compactMap { $0 as? DataSnapshot }.map { $0.key }
+            
+            // This group will keep track of the number of blocks still pending
+            let group = DispatchGroup()
+            
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let key = snap.key
+                let value = snap.value as! String
+                
+                self.hourOfDay.append(value)
+                
+                //print("key = \(key) value = \(value)")
+            }
+            
+            for groupKey in groupKeys {
+                group.enter()
+                self.ref?.child("groups").child(groupKey).child("name").observeSingleEvent(of: .value, with: { snapshot in
+                    group.leave()
+                })
+            }
+            
+            // We ask to be notified when every block left the group
+            group.notify(queue: .main) {
+                print("All callbacks are completed")
+                
+                self.lineChartProperties()
+                self.lineChartView.setBarChartData(xValues: self.hourOfDay, yValues: self.numberOfEntriesArray, label: "Number of Entries")
+            }
+        })
+    }
     
     func determineDay() -> String {
         let date = Date()
@@ -360,37 +135,30 @@ class HomeVC: UIViewController, ChartViewDelegate {
         var value = ""
         
         if day == 1 {
-            hourOfDay = ["8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:30"]
             value = "sunday"
         }
         
         else if day == 2 {
-            hourOfDay = ["6","7","8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
             value = "monday"
         }
         
         else if day == 3 {
-            hourOfDay = ["6:00","7:00","8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00"]
             value = "tuesday"
         }
         
         else if day == 4 {
-            hourOfDay = ["6:00","7:00","8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00"]
             value = "wednesday"
         }
         
         else if day == 5 {
-            hourOfDay = ["6:00","7:00","8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00"]
             value = "thursday"
         }
         
         else if day == 6 {
-            hourOfDay = ["6:00","7:00","8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00"]
             value = "friday"
         }
         
         else {
-            hourOfDay = ["8:00", "9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:30"]
             value = "saturday"
         }
         
